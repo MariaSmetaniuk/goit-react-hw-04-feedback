0df089bb58f60f1edd23426191c3d56756123b7f
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-10 20:37:22 +0300

    change Section file

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,23 +1,15 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
 import { Title } from './Section.styled';
 
-export class Section extends Component {
-  static defaultProps = {
-    title: 'My title',
-  };
-
-  render() {
-    const { title, children } = this.props;
-    return (
-      <Box py={4} as="section">
-        <Title>{title}</Title>
-        {children}
-      </Box>
-    );
-  }
-}
+export const Section = ({ title = 'My title', children }) => {
+  return (
+    <Box py={4} as="section">
+      <Title>{title}</Title>
+      {children}
+    </Box>
+  );
+};
 
 Section.propTypes = {
   title: PropTypes.string,
