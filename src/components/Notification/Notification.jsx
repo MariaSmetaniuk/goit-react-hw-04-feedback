9e0c50860e68a663936d365c79e0652b055cf980
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-10 20:31:48 +0300

    change notification file

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,13 +1,9 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NotificationStyled } from './Notification.styled';
 
-export class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return <NotificationStyled>{message}</NotificationStyled>;
-  }
-}
+export const Notification = ({ message }) => {
+  return <NotificationStyled>{message}</NotificationStyled>;
+};
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
