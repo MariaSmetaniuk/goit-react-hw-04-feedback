d579dc4b288a3b9680387b3bd15ea45cff57bd1b
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-10 20:35:41 +0300

    change StatisticsItem file

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,18 +1,14 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StatisticsItem } from './StatisticsItem.styled';
 
-export class Item extends Component {
-  render() {
-    const { name, value } = this.props;
-    return (
-      <StatisticsItem>
-        {name}
-        <span>{value}</span>
-      </StatisticsItem>
-    );
-  }
-}
+export const Item = ({ name, value }) => {
+  return (
+    <StatisticsItem>
+      {name}
+      <span>{value}</span>
+    </StatisticsItem>
+  );
+};
 
 Item.propTypes = {
   name: PropTypes.string.isRequired,
