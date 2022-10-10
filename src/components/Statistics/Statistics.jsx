10cfc23f370bb6d507426f4d2032b49f4972b79e
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-10 20:39:04 +0300

    change Statistics file

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,24 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { StatisticsList } from './Statistics.styled';
 import { Item } from '../StatisticsItem/StatisticsItem';
 
-export class Statistics extends Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <StatisticsList>
-        <Item name="Good:" value={good} />
-        <Item name="Neutral:" value={neutral} />
-        <Item name="Bad:" value={bad} />
-        <Item name="Total:" value={total} />
-        <Item name="Positive feedback:" value={`${positivePercentage}%`} />
-      </StatisticsList>
-    );
-  }
-}
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <StatisticsList>
+      <Item name="Good:" value={good} />
+      <Item name="Neutral:" value={neutral} />
+      <Item name="Bad:" value={bad} />
+      <Item name="Total:" value={total} />
+      <Item name="Positive feedback:" value={`${positivePercentage}%`} />
+    </StatisticsList>
+  );
+};
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
