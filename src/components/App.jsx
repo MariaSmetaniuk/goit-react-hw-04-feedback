2063f33915b7aeabe580332f71dc0e3bf89c99fa
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-11 08:52:11 +0300

    create options Object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const options = { good, neutral, bad };
 
   const countTotalFeedback = () => {
     return good + neutral + bad;
@@ -52,7 +53,7 @@ export const App = () => {
       >
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={Object.keys({ good, neutral, bad })}
+            options={Object.keys(options)}
             onLeaveFeedback={onLeaveFeedback}
           />
         </Section>
